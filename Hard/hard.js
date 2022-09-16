82f4e3fa6812bd851a12d25de0f6dfb4aaab9681
Ray T <112096459+TelRay701@users.noreply.github.com>
2022-09-16 12:35:43 -0400

    Cleaned up the code

--- Hard/hard.js
+++ Hard/hard.js
@@ -1,8 +1,3 @@
-// Create 4 math functions, one called add() that adds 2 numbers,
-// one called subtract() that subtracts 2 numbers,
-// one called multiply() to multiply 2 numbers, and
-// one called divide() to divide two numbers.
-
 function add(num1, num2) {
   let result = num1 + num2;
   return result;
